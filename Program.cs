1bbed5bd8fb7bb5b399ad2a686ca2834166f59d8
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-21 15:01:17 +0530

    Added Program to Print Stock Report

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Stock Account Management");
+
+            string path = "D:\\c#\\257\\StockAccountManagement\\StockAccountManagement\\Stocks.json";
+
+            FetchStock fetchStock = new FetchStock();
+
+            StockDetails data = fetchStock.Read(path);
+
+            Console.WriteLine("\nSTOCK VALUE: ");
+
+            for (int i = 0; i < data.stocks.Count; i++)
+            {
+                Console.WriteLine(data.stocks[i].StockName);
+                Console.WriteLine("shares: " + data.stocks[i].shares);
+                Console.WriteLine("price: " + data.stocks[i].Price);
+                Console.WriteLine("-------------------------------");
+            }
         }
     }
 }
